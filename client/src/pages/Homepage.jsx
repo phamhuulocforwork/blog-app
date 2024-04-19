795e5989461f2e66e2ec2efdd9a18acd081384be
HuuLoc <phamhuulocforwork@gmail.com>
2024-04-19 23:26:30 +0700

    Header

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Header from "../components/Header";
 
 const HomePage = () => {
-  return <div>This is home page</div>;
+  return (
+    <div className="w-full min-h-screen bg-blue-200 flex flex-col items-center">
+      <Header></Header>
+    </div>
+  );
 };
 
 HomePage.propTypes = {};
